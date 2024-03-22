cbb1c19721e34fb3154d8f452eadaa219f7a89c1
Phạm Sĩ Chiến <phamsichien112@gmail.com>
2024-03-22 20:05:03 +0700

    updateLinkDatabaseDeploy

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+    const conn = await mongoose.connect(process.env.MONGODB_DEPLOY_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
